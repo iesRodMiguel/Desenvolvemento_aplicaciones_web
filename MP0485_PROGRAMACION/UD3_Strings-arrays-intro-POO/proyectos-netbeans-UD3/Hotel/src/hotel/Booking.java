a387cbd1d7738d838569a15700817e2ffb9efc5b
iesRodMiguel <miguelmarmun@iesrodeira.gal>
2025-11-30 20:53:15 +0100

    updated documentos para consultas sobre clases más usadas en el curso

---
+++ MP0485_PROGRAMACION/UD3_Strings-arrays-intro-POO/proyectos-netbeans-UD3/Hotel/src/hotel/Booking.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hotel;
+
+/**
+ *
+ * @author miguel
+ */
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.LocalDateTime;
+
+public class Booking {
+
+    private int number;
+    private LocalDateTime startdate;
+    private LocalDateTime endddate;
+    private Client guest;
+    private Room room;
+    private boolean checkin;
+
+    
+    public Booking(){}
+
+
+
+}
